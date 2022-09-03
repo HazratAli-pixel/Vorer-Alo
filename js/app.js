706d0b807c516849add448781b35c16b839ed40f
HazratAli <hazrat369ali@gmail.com>
2022-09-04 03:47:07 +0600

    ten

--- js/app.js
+++ js/app.js
@@ -73,7 +73,52 @@ const allNewsDisplayCategory = async (event) =>{
     </div>`
     }
     newspart.innerHTML="";
-    data.map(data =>{
+    // we can use 'map()' function but for Video command, i use 'foreach()' function
+    // data.map(data =>{
+    //     const card_div = document.createElement("div");
+    //     card_div.classList.add('card','mb-3')    
+    //     card_div.innerHTML=`
+    // <div class="row g-0 test">
+    //   <div class="col-sm-12 col-lg-4 p-3">
+    //     <img src="${data.image_url}" class="img-fluid h-100 rounded" alt="">
+    //   </div>
+    //   <div class="col-sm-12 col-lg-8 position-relative">
+    //     <div class="card-body">
+    //       <h5 class="card-title">${data.title}</h5>
+    //       <p class="card-text">${ (data.details).length >=500 ? data.details.slice(0,300).concat("..."):data.details}
+    //       </p>
+    //     </div>
+    //     <div class="w-100 bg-white py-2 position-absolute bottom-0 start-0 d-flex flex-wrap justify-content-between align-items-center">
+    //       <div class="row w-100">
+    //         <div class="col-sm-12 col-md-6 col-lg-6 d-flex align-items-center justify-content-between">
+    //           <div class="ps-3 d-flex align-items-center justify-content-between">
+    //             <div class="">
+    //               <img style="width: 40px;" class="rounded-circle" src="${data.author?.img}" alt="">
+    //             </div>
+    //             <div class="ps-2">
+    //               <p class="m-0 fw-bolder">${data.author?.name =='' || data.author?.name == null? "Not Found":data.author?.name}</p>
+    //               <p class="m-0 text-muted">${data.author?.published_date?.slice(0,10)}</p>
+    //             </div>
+    //           </div>
+    //           <div>
+    //             <h5 class="m-0 pe-4"><i class="fa-solid fa-eye"></i> ${data?.total_view ==null || data?.total_view ==0? "NO View":data.total_view}</h5>
+    //           </div>
+    //         </div>
+
+    //         <div class="col-sm-12 col-md-6 col-lg-6 d-flex align-items-center justify-content-between">
+    //           <div class="ps-4"><span class="fa-solid fa-star"></span> ${data?.rating?.number}
+    //           </div>
+    //           <div class="pe-3">
+    //             <button class="btn btn-info" id="${data?._id}" onclick="newsdetails(event)" data-bs-toggle="modal" data-bs-target="#exampleModal" >Details</button>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    //   </div>` 
+    //     newspart.appendChild(card_div)
+    // })
+    data.forEach(data => {
         const card_div = document.createElement("div");
         card_div.classList.add('card','mb-3')    
         card_div.innerHTML=`
@@ -116,7 +161,7 @@ const allNewsDisplayCategory = async (event) =>{
       </div>
       </div>` 
         newspart.appendChild(card_div)
-    })
+    });
     return data;
 }
 
